4bf70b2b0e46c2255bf3eda5f5c9ed6eb0e3bec1
Teddy <dtd172000@gmail.com>
2023-06-15 20:56:23 +0700

    1st commit

--- pages/api/profile/[id].ts
+++ pages/api/profile/[id].ts
@@ -13,7 +13,7 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method === 'GET') {
-    const { id } = req.query;
+    const { id }: any = req.query;
 
     const query = singleUserQuery(id);
     const userVideosQuery = userCreatedPostsQuery(id);
